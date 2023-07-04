e8c004bcf67136b516157d4c0a5874b3ce43fd72
Jormund123 <anand.karna.social@gmail.com>
2023-07-04 13:16:07 +0545

    Setting Up The Environment

---
+++ app/(site)/page.tsx
@@ -0,0 +1,8 @@
+
+// folder name is (site): paranthesis is used so that it doesn't create another route for page.tsx
+
+export default function Home() {
+  return (
+    <div className = "text-green-500">Hello (!Site)</div>
+  )
+}
